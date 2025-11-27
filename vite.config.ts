63c90a1735e7aefa4e365c74a707611db86edff4
Chancelor Assiamah <chanceyassiamah@gmail.com>
2025-11-27 11:57:55 +0000

    CHORE: removed the proxy workaround. it was unnecessary and a vestige of when our backend architecture was wack af.

--- vite.config.ts
+++ vite.config.ts
@@ -5,26 +5,15 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    exclude: ['**/node_modules/**', '**/dist/**', '**/e2e/**'],
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      // Proxy requests from /api to your backend
-      // '/api': {
-      //   target: 'http://localhost:8001',
-      //   changeOrigin: true,
-      //   rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api from the path
-      // },
-      // Proxy requests from /mcp-api to your MCP server
-      '/mcp-api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/mcp-api/, '/mcp'), // Change path to /mcp
-      },
     },
   },
 })
